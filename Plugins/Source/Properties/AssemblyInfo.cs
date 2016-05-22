92b467bbee30513ea7a2bf8bfc31a7a9619486c2
ihsoft <igor.zavoychinskiy@gmail.com>
2016-05-21 21:35:53 -0700

    Update to KSPDev Utils 0.12.0

--- Plugins/Source/Properties/AssemblyInfo.cs
+++ Plugins/Source/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.2.9")]
 [assembly: AssemblyInformationalVersion("1.2.9 for KSP 1.1")]
 [assembly: KSPAssembly("KIS", 1, 2)]
-[assembly: KSPAssemblyDependency("KSPDev_Utils", 0, 10)]
+[assembly: KSPAssemblyDependency("KSPDev_Utils", 0, 12)]
