4bbc27799a0345b3703cc36e84e0461cdf39ce0c
ihsoft <igor.zavoychinskiy@gmail.com>
2017-08-29 21:34:47 -0700

    Restore start/stop ability
    Plus using spatial sound library for the audio clip.

--- Source/ModuleKISItemSoundPlayer.cs
+++ Source/ModuleKISItemSoundPlayer.cs
@@ -4,6 +4,7 @@
 // License: Restricted
 
 using KSPDev.GUIUtils;
+using KSPDev.SoundsUtils;
 using System;
 using System.Linq;
 using UnityEngine;
@@ -16,35 +17,26 @@ public sealed class ModuleKISItemSoundPlayer : ModuleKISItem {
   public string sndPath = "KIS/Sounds/guitar";
   [KSPField]
   public float sndMaxDistance = 10;
-  public FXGroup sndFx;
   [KSPField]
   public bool loop;
   #endregion
 
+  public AudioSource sndMainTune;
+
   public override void OnStart(StartState state) {
     base.OnStart(state);
-    if (state == StartState.None) {
-      return;
-    }
-    if (HighLogic.LoadedSceneIsFlight) {
-      sndFx.audio = part.gameObject.AddComponent<AudioSource>();
-      sndFx.audio.volume = GameSettings.SHIP_VOLUME;
-      sndFx.audio.rolloffMode = AudioRolloffMode.Linear;
-      sndFx.audio.dopplerLevel = 0f;
-      sndFx.audio.spatialBlend = 1f;
-      sndFx.audio.maxDistance = sndMaxDistance;
-      sndFx.audio.loop = loop;
-      sndFx.audio.playOnAwake = false;
-      sndFx.audio.clip = GameDatabase.Instance.GetAudioClip(sndPath);
+    if (state != StartState.None && HighLogic.LoadedSceneIsFlight) {
+      sndMainTune = SpatialSounds.Create3dSound(
+          gameObject, sndPath, loop: loop, maxDistance: sndMaxDistance);
     }
   }
 
   public override void OnItemUse(KIS_Item item, KIS_Item.UseFrom useFrom) {
     if (useFrom != KIS_Item.UseFrom.KeyUp) {
-      if (item.inventory.sndFx.audio.isPlaying) {
-        item.inventory.sndFx.audio.Stop();
+      if (sndMainTune.isPlaying) {
+        sndMainTune.Stop();
       } else {
-        UISoundPlayer.instance.Play(sndPath);
+        sndMainTune.Play();
       }
     }
   }
@@ -52,10 +44,10 @@ public sealed class ModuleKISItemSoundPlayer : ModuleKISItem {
   [KSPEvent(name = "ContextMenuPlay", guiActiveEditor = false, active = true, guiActive = true,
             guiActiveUnfocused = true, guiName = "Play")]
   public void Play() {
-    if (sndFx.audio.isPlaying) {
-      sndFx.audio.Stop();
+    if (sndMainTune.isPlaying) {
+      sndMainTune.Stop();
     } else {
-      sndFx.audio.Play();
+      sndMainTune.Play();
     }
   }
 }
