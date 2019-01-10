49585bfd7d3b2f2d75eee61f01c233f0b73e95db
ihsoft <igor.zavoychinskiy@gmail.com>
2019-01-09 22:19:37 -0800

    Add a starter of KIS API

---
+++ Source/api/Utils/ColliderUtilsImpl.cs
@@ -0,0 +1,55 @@
+﻿// Kerbal Inventory System
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KISAPIv1 {
+
+/// <summary>Various methods to deal with the part's colliders.</summary>
+public class ColliderUtilsImpl {
+
+  /// <summary>
+  /// Returns the minimum square distance to the nearest point on the part's collider surface.
+  /// </summary>
+  /// <remarks>This method skips triggers and inactive collidres.</remarks>
+  /// <param name="point">The reference point to find distance for.</param>
+  /// <param name="part">The part to check for.</param>
+  /// <param name="filterFn">
+  /// The filter function to apply to every collider. Return <c>false</c> from it to sklipp the
+  /// collider in the following checks.
+  /// </param>
+  /// <returns>The square distance or <c>null</c> if no colliders found.</returns>
+  public float? GetSqrDistanceToPart(
+      Vector3 point, Part part, Func<Collider, bool> filterFn = null) {
+    return part.transform.GetComponentsInChildren<Collider>()
+        .Where(c => !c.isTrigger && c.enabled && c.gameObject.activeInHierarchy
+             && (filterFn == null || filterFn(c)))
+        .Select(c => c.ClosestPoint(point))
+        .Min(pt => (float?)(pt - point).sqrMagnitude);
+  }
+
+  /// <summary>
+  /// Returns the minimum square distance to the nearest point on the part's collider surface.
+  /// </summary>
+  /// <remarks>This method skips triggers and inactive collidres.</remarks>
+  /// <param name="point">The reference point to find distance for.</param>
+  /// <param name="part">The part to check for.</param>
+  /// <param name="defaultValue">The value to return if no suitable colliders found.</param>
+  /// <param name="filterFn">
+  /// The filter function to apply to every collider. Return <c>false</c> from it to sklipp the
+  /// collider in the following checks.
+  /// </param>
+  /// <returns>
+  /// The square distance or <paramref name="defaultValue"/> if no colliders found.
+  /// </returns>
+  public float GetSqrDistanceToPartOrDefault(Vector3 point, Part part,
+                                             float defaultValue = float.PositiveInfinity,
+                                             Func<Collider, bool> filterFn = null) {
+    return GetSqrDistanceToPart(point, part) ?? defaultValue;
+  }
+}
+
+}  // namespace
---
+++ Source/api/Utils/KISAPI.cs
@@ -0,0 +1,31 @@
+﻿// Kerbal Inventory System API
+// API design and implemenation: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+//
+// This module is used to build the API assembly. Once released to the public the version of this
+// assembly cannot change. This guarantees that all the dependent mods will be able to load it. In
+// case of a new version of API is released it must inherit from the previous version, and become a
+// completely new assembly that is supplied together with the old versions.
+//
+// It's unspecified how many old versions of the API are to be preserved in the distribution. Mod
+// developers should migrate to the newest available API version as soon as possible. In a normal
+// case, each version is an ancestor of the previous version(s), so the migration
+// should be trivial. However, in case of there is some methods/interfaces deprecation, an extra
+// work may be required to migrate.
+
+// FIXME: For now it's only a dummy! A lot of refactoring is needed before we get KIS API.
+
+// Name of the namespace denotes the API version.
+namespace KISAPIv1 {
+
+/// <summary>KIS API, version 1.</summary>
+/// FIXME: This implementation is a FAKE for now. It's not a real API.
+public static class KISAPI {
+  /// <summary>Tells if API V1 was loaded and ready to use.</summary>
+  public static bool isLoaded = true;
+
+  /// <summary>Untils to deal with colliders within KIS.</summary>
+  public static ColliderUtilsImpl colliderUtils = new ColliderUtilsImpl();
+}
+
+}  // namespace
