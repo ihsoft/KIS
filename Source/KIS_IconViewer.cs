5079372984649e1b0e4f38d3bd4faec6978b4719
ihsoft <igor.zavoychinskiy@gmail.com>
2019-01-16 20:04:38 -0800

    Style refactor
    - Hide members that are not used outside.
    - Use PartUtils to make the iconPrefab.
    - Some style changes.

--- Source/KIS_IconViewer.cs
+++ Source/KIS_IconViewer.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using KISAPIv1;
+using System;
 using System.Linq;
 using UnityEngine;
 
 namespace KIS {
 
-public class KIS_IconViewer : IDisposable {
+public sealed class KIS_IconViewer : IDisposable {
   const float iconPosY = 0;
   const int mask = 22;
   const float lightIntensity = 0.4f;
@@ -16,84 +17,21 @@ public class KIS_IconViewer : IDisposable {
   static int camStaticIndex;
   static int iconCount;
   int camIndex;
+  GameObject iconPrefab;
 
-  public GameObject iconPrefab;
-  public Texture texture;
+  public Texture texture { get; private set; }
 
-  public KIS_IconViewer(Part p, int resolution) {
-    if (p.vessel != null && p.vessel.isEVA) {
-      // Icon Camera
-      GameObject camGo = new GameObject("KASCamItem" + camStaticIndex);
-      camGo.transform.parent = p.transform;
-      camGo.transform.localPosition = Vector3.zero + new Vector3(0, 0.35f, 0.7f);
-      camGo.transform.localRotation = Quaternion.identity;
-      camGo.transform.Rotate(0.0f, 180f, 0.0f);
-      cam = camGo.AddComponent<Camera>();
-      cam.orthographic = true;
-      cam.orthographicSize = 0.35f;
-      cam.clearFlags = CameraClearFlags.Color;
-      // Render texture
-      RenderTexture tex = new RenderTexture(resolution, resolution, 8);
-      this.texture = tex;
-
-      cam.cullingMask = Camera.main.cullingMask;
-      cam.farClipPlane = 1f;
-
-      // Texture
-      cam.targetTexture = tex;
-      cam.ResetAspect();
+  public KIS_IconViewer(Part part, int resolution) {
+    if (part.vessel != null && part.vessel.isEVA) {
+      MakeKerbalAvatar(part, resolution);
     } else {
-      // Instantiate part icon
-      iconPrefab = UnityEngine.Object.Instantiate((UnityEngine.Object)p.partInfo.iconPrefab)
-          as GameObject;
-
-      // Command Seat Icon Fix (Temporary workaround until squad fix the broken shader)
-      Shader fixShader = Shader.Find("KSP/Alpha/Cutoff Bumped");
-      foreach (Renderer r in iconPrefab.GetComponentsInChildren<Renderer>(true)) {
-        foreach (Material m in r.materials) {
-          if (m.shader.name == "KSP/Alpha/Cutoff") {
-            m.shader = fixShader;
-          }
-        }
-      }
-
-      iconPrefab.SetActive(true);
-
-      // Icon Camera
-      GameObject camGo = new GameObject("KASCamItem" + camStaticIndex);
-      camGo.transform.position = new Vector3(camStaticIndex, iconPosY, 0);
-      camGo.transform.rotation = Quaternion.identity;
-      cam = camGo.AddComponent<Camera>();
-      cam.orthographic = true;
-      cam.orthographicSize = zoom;
-      cam.clearFlags = CameraClearFlags.Color;
-      cam.enabled = false;
-      // Render texture
-      RenderTexture tex = new RenderTexture(resolution, resolution, 8);
-      this.texture = tex;
-
-      //light
-      if (iconLight == null && HighLogic.LoadedSceneIsFlight) {
-        GameObject lightGo = new GameObject("KASLight");
-        iconLight = lightGo.AddComponent<Light>();
-        iconLight.cullingMask = 1 << mask;
-        iconLight.type = LightType.Directional;
-        iconLight.intensity = lightIntensity;
-      }
-
-      // Layer
-      cam.cullingMask = 1 << mask;
-      SetLayerRecursively(iconPrefab, mask);
-
-      // Texture
-      cam.targetTexture = tex;
-      cam.ResetAspect();
-
-      // Cam index
-      this.camIndex = camStaticIndex;
-      camStaticIndex += 2;
-      ResetPos();
+      MakePartIcon(part.partInfo, resolution, KISAPI.PartUtils.GetCurrentPartVariant(part));
     }
+    iconCount += 1;
+  }
+
+  public KIS_IconViewer(AvailablePart avPart, int resolution, PartVariant variant) {
+    MakePartIcon(avPart, resolution, variant);
     iconCount += 1;
   }
 
@@ -107,14 +45,14 @@ public class KIS_IconViewer : IDisposable {
   // because we can only access the cam.gameObject member from the main thread.
   public void Dispose()
   {
-    if (cam) {
-        cam.gameObject.DestroyGameObject();
+    if (cam != null) {
+      cam.gameObject.DestroyGameObject();
+      cam = null;
     }
-    if (iconPrefab) {
+    if (iconPrefab != null) {
       iconPrefab.DestroyGameObject();
+      iconPrefab = null;
     }
-    cam = null;
-    iconPrefab = null;
     texture = null;
     iconCount -= 1;
     if (iconCount == 0) {
@@ -135,10 +73,11 @@ public class KIS_IconViewer : IDisposable {
     cam.Render();  // Update snapshot.
   }
 
-  public static void ResetCamIndex() {
+  static void ResetCamIndex() {
     camStaticIndex = 0;
   }
 
+  #region Local utility methods
   void SetLayerRecursively(GameObject obj, int newLayer) {
     if (null == obj) {
       return;
@@ -151,6 +90,79 @@ public class KIS_IconViewer : IDisposable {
       SetLayerRecursively(child.gameObject, newLayer);
     }
   }
+
+  void MakeKerbalAvatar(Part ownerPart, int resolution) {
+    // Icon Camera
+    GameObject camGo = new GameObject("KASCamItem" + camStaticIndex);
+    camGo.transform.parent = ownerPart.transform;
+    camGo.transform.localPosition = Vector3.zero + new Vector3(0, 0.35f, 0.7f);
+    camGo.transform.localRotation = Quaternion.identity;
+    camGo.transform.Rotate(0.0f, 180f, 0.0f);
+    cam = camGo.AddComponent<Camera>();
+    cam.orthographic = true;
+    cam.orthographicSize = 0.35f;
+    cam.clearFlags = CameraClearFlags.Color;
+    // Render texture
+    RenderTexture tex = new RenderTexture(resolution, resolution, 8);
+    texture = tex;
+
+    cam.cullingMask = Camera.main.cullingMask;
+    cam.farClipPlane = 1f;
+
+    // Texture
+    cam.targetTexture = tex;
+    cam.ResetAspect();
+  }
+
+  void MakePartIcon(AvailablePart avPart, int resolution, PartVariant variant) {
+    // Instantiate part icon
+    iconPrefab = KISAPI.PartUtils.GetIconPrefab(avPart, variant);
+
+    // Command Seat Icon Fix (Temporary workaround until squad fix the broken shader)
+    Shader fixShader = Shader.Find("KSP/Alpha/Cutoff Bumped");
+    foreach (Renderer r in iconPrefab.GetComponentsInChildren<Renderer>(true)) {
+      foreach (Material m in r.materials) {
+        if (m.shader.name == "KSP/Alpha/Cutoff") {
+          m.shader = fixShader;
+        }
+      }
+    }
+
+    // Icon Camera
+    GameObject camGo = new GameObject("KASCamItem" + camStaticIndex);
+    camGo.transform.position = new Vector3(camStaticIndex, iconPosY, 0);
+    camGo.transform.rotation = Quaternion.identity;
+    cam = camGo.AddComponent<Camera>();
+    cam.orthographic = true;
+    cam.orthographicSize = zoom;
+    cam.clearFlags = CameraClearFlags.Color;
+    cam.enabled = false;
+    // Render texture
+    RenderTexture tex = new RenderTexture(resolution, resolution, 8);
+    texture = tex;
+
+    //light
+    if (iconLight == null && HighLogic.LoadedSceneIsFlight) {
+      GameObject lightGo = new GameObject("KASLight");
+      iconLight = lightGo.AddComponent<Light>();
+      iconLight.cullingMask = 1 << mask;
+      iconLight.type = LightType.Directional;
+    }
+
+    // Layer
+    cam.cullingMask = 1 << mask;
+    SetLayerRecursively(iconPrefab, mask);
+
+    // Texture
+    cam.targetTexture = tex;
+    cam.ResetAspect();
+
+    // Cam index
+    camIndex = camStaticIndex;
+    camStaticIndex += 2;
+    ResetPos();
+  }
+  #endregion
 }
 
 }  // namespace
