d4d898fae811b110107d2580f0e5b350e76739ae
ihsoft <igor.zavoychinskiy@gmail.com>
2019-02-02 00:13:04 -0800

    Release v1.18

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("1.18.*")]
-[assembly: AssemblyInformationalVersion ("1.18-pre for KSP 1.6+")]
+[assembly: AssemblyInformationalVersion ("1.18 for KSP 1.6+")]
 [assembly: KSPAssembly ("KIS", 1, 18)]
